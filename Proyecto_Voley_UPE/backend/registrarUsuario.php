6c47ef87918ab2b4187051024f84418972178df7
Elias Contrera <florencioeliascontrera@gmail.com>
2023-10-22 22:41:36 -0300

    Actualizacion: Backend
    - Login de usuarios
    - Utilizacion de BBDD (phpmyadmin)
    - Agregado de Archivos de Noticias
    - Cierre e Inicio de Sesion/Registrarse

---
+++ Proyecto_Voley_UPE/backend/registrarUsuario.php
@@ -0,0 +1,23 @@
+
+<?php
+include_once ('clases/Usuario.php');
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $correo = $_POST["email"];
+    $password = $_POST["passwordRegistro"];
+    $fecha_edad = $_POST["fecha_edad"];
+    $perfil = 3;
+    
+    $usuario = new Usuario($correo, $password,$fecha_edad, $perfil);
+
+   $resultado = $usuario->registrar();
+   if($resultado == true)
+   {
+    header('Location: ../frontend/index.html');
+    exit();
+   }
+}
+?>
+
+
